c7cc546a66cc943bce86ffbe73ad7422b300a5f5
RagvirH <157735804+RagvirH@users.noreply.github.com>
2024-11-15 18:33:34 +0000

    part 3

---
+++ blogproject/blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import BlogPost
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'content']  # Add any other fields you want in your form
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import BlogPost
+from .forms import PostForm
 
-# Create your views here.
+class PostListView(ListView):
+    model = BlogPost
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+class PostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    model = BlogPost
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+class PostUpdateView(UpdateView):
+    model = BlogPost
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
